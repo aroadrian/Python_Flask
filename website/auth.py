48ad5f4f7cbcce6fd8284c9b94eff2363eefc3bf
Adrian Aro <aroadrianchristopher@gmail.com>
2025-02-12 20:46:01 +0800

    basic setup routes in flask

---
+++ website/auth.py
@@ -0,0 +1,15 @@
+from flask import Blueprint
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return "<h1>Hello Auth!</h1>"
+
+@auth.route('/logout')
+def logout():
+    return "<h1>Goodbye Auth!</h1>"
+
+@auth.route('/sign-up')
+def sign_up():
+    return "<h1>Sign Up!</h1>"
