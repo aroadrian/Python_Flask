5a1fd127a3eaf38219c639394b446fffc1915629
Adrian Aro <aroadrianchristopher@gmail.com>
2025-02-25 22:21:04 +0800

    try to update the code

--- main.py
+++ main.py
@@ -3,4 +3,16 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(debug=True)
+#from flask import Flask, render_template 
+
+#app = Flask(__name__)   
+
+
+#@app.route('/')
+#def Index():  
+ #   return render_template('index.html')  
+
+
+#if __name__ == '__main__':  
+  #  app.run(debug=True)
